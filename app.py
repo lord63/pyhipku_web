d829cdaa1702730d8d832046af76e076198fcf1d
lord63 <lord63.j@gmail.com>
2015-03-09 16:19:46 +0800

    Show your original IP

--- app.py
+++ app.py
@@ -10,21 +10,23 @@ from pyhipku import encode
 app = Flask(__name__)
 
 
-@app.route('/<ip>')
-def index(ip):
-    lines = encode(ip).split('\n')
-    return render_template('cover.html', lines=lines)
+@app.route('/<current_ip>')
+def index(current_ip):
+    your_ip = request.remote_addr
+    lines = encode(current_ip).split('\n')
+    return render_template('cover.html', lines=lines, your_ip=your_ip,
+                           current_ip=current_ip)
 
 
 @app.route('/')
 def get_ip():
-    return redirect(url_for('index', ip=request.remote_addr))
+    return redirect(url_for('index', current_ip=request.remote_addr))
 
 
 @app.route('/random')
 def random_ip():
     random_ip = '.'.join(map(str, [randint(0, 255) for _ in range(4)]))
-    return redirect(url_for('index', ip=random_ip))
+    return redirect(url_for('index', current_ip=random_ip))
 
 
 if __name__ == '__main__':
